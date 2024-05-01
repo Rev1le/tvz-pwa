a7dede36134caa7978c8e353a666c06a21a7dfe8
Rev1le <r.nikiy@yandex.ru>
2024-05-02 02:14:22 +0500

    Fix imports

--- src/widgets/filters/index.js
+++ src/widgets/filters/index.js
@@ -1,4 +1,6 @@
-import { ClientFilter } from './ui/ClientFilter.vue';
-import { OrderTypeFilter } from './ui/OrderTypeFilter.vue';
+import ClientFilter from './ui/ClientFilter.vue';
+import OrderTypeFilter from './ui/OrderTypeFilter.vue';
 
-export { ClientFilter, OrderTypeFilter }
+import { useFilterStore } from './model/store'
+
+export { ClientFilter, OrderTypeFilter, useFilterStore }
